859ee2d16ba4051143df5f7f52e1c425e924dc11
Edmar Ondevilla <142650971+OndevillaE2mar@users.noreply.github.com>
2023-10-14 09:51:55 +0800

    Add files via upload
    Signed-off-by: Edmar Ondevilla <142650971+OndevillaE2mar@users.noreply.github.com>

---
+++ newfile.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Alcala {
+ public static void main(String[] args) {
+  
+     Scanner in = new Scanner (System.in) ;
+       int Num1, Num2, Num3, Num4;
+  
+     System.out.println ("Enter num1");
+       Num1= in.nextInt();
+    
+     System.out.println ("Enter num2");
+       Num2 = in.nextInt();
+     
+      System.out.println ("Enter num3");
+       Num3 = in.nextInt();
+     
+      System.out.println ("Enter num4");
+       Num4 = in.nextInt();
+       
+    System.out.println ("Enter Operator");
+    System.out.println ("[+] [*]");
+    System.out.println ("[/] [-]");
+    char operator = in.next (). charAt (0) ;
+     switch (operator) {
+     case '+':
+     int add = Num1+Num2+Num3+Num4 ;
+        System.out.println ("Num1+Num2+Num3+Num4=" + add);
+         break;
+             case '*':
+               int mul = Num1*Num2*Num3*Num4 ;
+         System.out.println ("Num1*Num2*Num3*Num4=" + mul);
+         break;
+             case '/':
+               int div = Num1/Num2/Num3/Num4 ;
+         System.out.println ("Num1/Num2/Num3/Num4=" + div);
+         break;
+             case '-':
+               int sub = Num1-Num2-Num3-Num4 ;
+         System.out.println ("Num1-Num2-Num3-Num4=" + sub);
+         break;      
+          default :
+            System.out.println ("Invalid Input") ;  
+     
+    }
+  }
+}
